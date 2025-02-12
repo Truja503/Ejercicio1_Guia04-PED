edc59b9a31c86864b31693f5a7de5a57eacab604
IselMetzi <imetzicarrillom@gmail.com>
2025-02-11 20:28:55 -0600

    Clases Cliente y cola cliente

---
+++ Ejercicio1_Guia04 PED/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Guia04_PED
+{
+    public class Cliente
+    {
+        //atributos
+        public string nom;
+        public string apell;
+        public Cliente clientesig = null;
+
+        //metodos
+        public Cliente(string nom, string apell)
+        {
+            this.nom = nom;
+            this.apell = apell;
+    
+
+        }
+
+        public string NombreCompleto()
+        {
+            return(nom + " " + apell);
+        }
+    }
+}
---
+++ Ejercicio1_Guia04 PED/ColaCliente.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Ejercicio1_Guia04_PED
+{
+    public class ColaCliente
+    {
+        //atributos
+        Cliente clienteInic = null;
+        Cliente clienteFin = null;
+        int totalClientes = 0;
+
+        public Cliente ClienteInic 
+        {
+            get {  return clienteInic; } 
+            set {  clienteInic = value; }
+        }
+        public Cliente ClienteFin
+        {
+            get { return clienteFin; }
+            set { clienteInic = value; }
+
+        }
+        public int TotalClientes
+        {
+            get { return totalClientes; }
+            set
+            {
+                if (totalClientes >= 0)
+                {
+                    totalClientes = value;
+                }
+            }
+        }
+
+        //metodos
+        //metodo constructor
+        public ColaCliente()
+        {
+            ClienteInic = null;
+            ClienteFin = null;
+            TotalClientes = 0;
+        }
+
+        public int TotalCLientes()
+        {
+            return TotalClientes;
+        }
+
+       public void AgregarCli(Cliente nodo)
+        {
+            if( clienteInic == null )
+            {
+                ClienteInic = nodo;
+                ClienteFin = nodo;
+            }
+            else
+            {
+                ClienteFin.clientesig = nodo;
+                ClienteInic = nodo;
+
+            }
+            totalClientes++;
+        }
+
+        public Cliente RemoverCli()
+        {
+            Cliente aux = null;
+            if(ClienteInic != null)
+            {
+                aux = clienteInic;
+                clienteInic = clienteInic.clientesig;
+                totalClientes--;
+            }
+           return aux;
+        }
+
+        public void AnotarClientesEn(ListBox VistaClientes)
+        {
+            if (VistaClientes == null)
+            {
+                return; // Evita errores si la referencia es nula
+            }
+
+            Cliente actual = ClienteInic;
+
+            while (actual != null)
+            {
+                VistaClientes.Items.Add(actual.NombreCompleto());
+                actual = actual.clientesig;
+            }
+        }
+    }
+    
+}
